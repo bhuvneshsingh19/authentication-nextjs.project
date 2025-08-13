072675055be17f7776ed18763bd15f58f92fcda0
Bhuvnesh Singh <bhuvnesh.singh_cs.aiml22@gla.ac.in>
2025-08-13 23:19:37 +0530

    change in file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,6 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-  { files: ["**/*.ts", "**/*.tsx"], rules: { "@typescript-eslint/no-explicit-any": "warn", }, },
-
 ];
 
 
